dcea73b3b4002d33bff1f8b40d602d939eb83572
Mike Gering <mgering@freenas9-next.gattis>
2017-05-09 19:25:24 -0400

    wip

--- transmission_vpn_monitor.py
+++ transmission_vpn_monitor.py
@@ -1,13 +1,27 @@
 #!/usr/local/bin/python2.7
 
 import os
+from subprocess import Popen, PIPE
 
 def get_tun_ip():
-    f = os.popen('ifconfig tun0 | grep "inet\ " | cut -d: -f2 | cut -d" " -f2')
-    return f.read()
+    pipe = Popen("ifconfig tun0", shell=True, stdout=PIPE, stderr=PIPE).stdout
+    x = pipe.read()
+    if len(x) == 0:
+        return None
+    return x.split("inet ")[1].split(" ")[0]
+
+def stop_transmission():
+    pipe = Popen("service transmission stop", shell=True, stdout=PIPE, stderr=PIPE).stdout
+
+
 
 def run():
     tun_ip = get_tun_ip()
+    if tun_ip is None:
+        stop_transmission()
+    else:
+        # Check that the tunnel IP matches the transmission config
+        pass
     print(tun_ip)
 
 if __name__ == "__main__":
