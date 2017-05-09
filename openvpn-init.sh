58fb33d58c36f04845023a0cd5f611383a485fbe
Mike Gering <mgering@freenas9-next.gattis>
2017-05-09 17:10:43 -0400

    wip

--- openvpn-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# /etc/rc.conf needs these:
-#openvpn_enable="YES"
-#openvpn_configfile="/openvpn/default.ovpn"
-#openvpn_flags="--script-security 2"
-
-echo "Setting rc.conf for openvpn"
-
