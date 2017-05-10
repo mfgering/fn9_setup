79b22265cd31143324d86aca7dbf2fa6acd805df
Mike Gering <mike@gering.name>
2017-05-10 14:23:00 -0400

    wip

--- in_jail.py
+++ in_jail.py
@@ -1,9 +1,26 @@
 #!/usr/local/bin/python2.7
 import sys
 
-from utils import update_or_add_settings
+from utils import edit_rc_conf_settings
 
-def _add_transmission(conf_filename, write_file=False):
+def _edit_rc_conf(conf_filename, settings, write_file, add_conf):
+    with open(conf_filename) as f:
+        contents = f.read()
+    contents = edit_rc_conf_settings(contents, settings, add_conf)
+    if write_file:
+        with open(conf_filename, 'w') as f:
+            f.write(contents)
+    return contents
+
+def _edit_openvpn_rc_conf(conf_filename, write_file=False, add_conf=True):
+    settings = [
+        ('openvpn_enable', '"YES"'),
+        ('openvpn_configfile', '"/openvpn/default.ovpn"'),
+        ('openvpn_flags', '"--script-security 2"'),
+    ]
+    return _edit_rc_conf(conf_filename, settings, write_file, add_conf)
+
+def _add_transmission_rc_conf(conf_filename, write_file=False):
     settings = [
         ('transmission_enable', '"YES"'),
         ('transmission_conf_dir', '"/config"'),
@@ -11,20 +28,18 @@ def _add_transmission(conf_filename, write_file=False):
         ('transmission_watch_dir', '"/watched"'),
         ('transmission_user', '"media"'),
     ]
-    with open(conf_filename) as f:
-        contents = f.read()
-    contents = update_or_add_settings(contents, settings)
-    if write_file:
-        with open(conf_filename, 'w') as f:
-            f.write(contents)
-    return contents
+    return _edit_rc_conf(conf_filename, settings, write_file, add_conf)
 
 ######
 # Commands
 ######
 
+def add_openvpn_rc_conf(*args):
+    _edit_openvpn_rc_conf('/etc/rc.conf', write_file=True)
+
 def add_transmission_rc_conf(*args):
-    _add_transmission('/etc/rc.conf', True)
+    _add_transmission_rc_conf('/etc/rc.conf', write_file=True)
+
 
 def test(args):
     return 42
